e77006419fc5bd811d639bf031af7f3b935a6223
rubayatrifat <rubayatislam17@gmail.com>
2026-06-25 21:22:38 +0600

    JS problem 13 (25 June 2026)

--- problem-13.js
+++ problem-13.js
@@ -1,11 +1,14 @@
 /*
 
-Given the string representations of two integers, return the string representation of the sum of those integers.
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
 
 For example:
 
-sumStrings('1','2') // => '3'
-A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+Output: 6 (Sum of [4, -1, 2, 1])
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead. Your solution should be fast, it will be tested on very large arrays so slow solutions will time out.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
 
 */
 
