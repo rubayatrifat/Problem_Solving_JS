03b739c63780a0d2687d25eeb7c31a0dd432a558
rubayatrifat <rubayatislam17@gmail.com>
2026-06-27 10:54:41 +0600

    JS problem 14 (26 June 2026)

--- problem-14.js
+++ problem-14.js
@@ -1,7 +1,47 @@
 /*
 
+yrics...
+Pyramids are amazing! Both in architectural and mathematical sense. If you have a computer, you can mess with pyramids even if you are not in Egypt at the time. For example, let's consider the following problem: imagine that you have a pyramid built of numbers, like the ones here:
+
+    3                10
+   7 4             10  20
+  2 4 6          10  10  20
+ 8 5 9 3       10  90  10  20
+Here comes the task...
+Let's say that the 'slide down' is the maximum sum of consecutive numbers from the top to the bottom of the pyramid. As you can see, the largest 'slide downs' are 3 + 7 + 4 + 9 = 23, and 10 + 20 + 10 + 90 = 130.
+
+Your task is to write a function that takes a pyramid representation as an argument and returns its largest 'slide down'. For example:
+
+With the input [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]], your function should return 23.
+With the input [[10], [10, 20], [10, 10, 20], [10, 90, 10, 20]], your function should return 130.
+By the way...
+Tests include some extraordinarily high pyramids so as you can guess, brute-force method is a bad idea unless you have a few centuries to waste. You must come up with something more clever than that.
+
+(c) This task is a lyrical version of Problem 18 and/or Problem 67 on ProjectEuler.
 
 */
 
 
 
+/// GREADY Method
+function longestSlideDown(pyramid) {
+    let resultSum = pyramid[0][0]
+    let currentIndex = 0
+    for (let i = 1; i < pyramid.length; i++) {
+        if (pyramid[i][currentIndex] > pyramid[i][currentIndex + 1]) {
+            resultSum += pyramid[i][currentIndex]
+        } else {
+            resultSum += pyramid[i][currentIndex + 1]
+            currentIndex++
+        }
+    }
+    return resultSum;
+}
+
+
+console.log(longestSlideDown([
+    [3],
+    [7, 4],
+    [2, 4, 6],
+    [8, 5, 9, 3]]
+))
